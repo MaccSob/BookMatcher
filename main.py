928e0e27cf2017aab3b9063ceb8a6cb6954eb110
MaccSob <71sobmaciej@gmail.com>
2026-09-02 00:00:28 +0200

    another function

--- main.py
+++ main.py
@@ -24,11 +24,27 @@ def get_book_details(work_key):
         'description': data2.get('description')
     }
 
+def search_books(query, limit=5):
+    url = f"https://openlibrary.org/search.json?q={query}"
+    response = requests.get(url)
+    data = response.json()
+    all_docs = data['docs']
+    top_docs = all_docs[:limit]
+    results = []
+    for doc in top_docs:
+        work_key = doc.get('key')
+        dork = get_book_details(work_key)
+        results.append(dork)
+    return results
+
+
+
 
 
 wynik = get_book_details("/works/OL1168083W")
 surowe_subjects = wynik['subjects']
 wyczyszczone = clean_subjects(surowe_subjects)
+search_books("dune", 3),
 print(len(surowe_subjects))
 print(len(wyczyszczone))
 print(wyczyszczone)
